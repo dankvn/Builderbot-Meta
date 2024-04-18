1f6655d90b1d227ed94ea15b8351d594235c2747
kvndan <dannyelctro@gmail.com>
2024-04-18 10:59:28 -0500

    builderbot✅

--- src/flows/flowRegistered.js
+++ src/flows/flowRegistered.js
@@ -1,13 +1,26 @@
 import { addKeyword } from '@builderbot/bot';
 
+import flowmenu from "./flowmenu.js"
+import flowerro from "./flowerro.js"
+
+import GoogleSheetService from "./services/sheets/index.js";
+const googlesheet = new GoogleSheetService(
+    "1sjSk6t983zc9ZeojTdiLn67tN4W854Ekcjq75Dwfga8"
+  );
+
 const flowRegistered = addKeyword('register')
-  .addAnswer('What is your name?', { capture: true }, async (ctx, { state }) => {
-    const responseName = ctx.body
-    await state.update({ name: responseName })
-  })
-  .addAnswer('What is your email?', { capture: true }, async (ctx, { state }) => {
-    const responseEmail = ctx.body
-    await state.update({ email: responseEmail })
-  })
+.addKeyword("USUARIOS_REGISTRADOS")
+.addAction(async (ctx, { flowDynamic, gotoFlow }) => {
+  const telefono = ctx.from;
+  const ifExist = await googlesheet.validatePhoneNumber(telefono);
+  const mensaje = `👋Hola ${ifExist.Nombre}, soy tu asistente virtual `;
+
+  await flowDynamic(mensaje);
+  if (ifExist) {
+    return gotoFlow(flowmenu); // Si está registrado, ir al flujo de registrados
+  } else {
+    return gotoFlow(flowerro); // Si no está registrado, ir al flujo de no registrados
+  }
+});
 
 export default flowRegistered 
