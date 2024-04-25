e01314833dd021a043437a10225d839604590aa1
kvndan <dannyelctro@gmail.com>
2024-04-25 10:39:53 -0500

    builderbot1✅

--- src/flows/flowUserNotRegistered.js
+++ src/flows/flowUserNotRegistered.js
@@ -1,14 +1,17 @@
 import { addKeyword, EVENTS } from '@builderbot/bot';
-import flowRegistered from '../flows/flowRegistered.js';
+import flowmenu from './flowmenu.js';
 
-  const flowUserNotRegistered= addKeyword(EVENTS.ACTION).addAnswer(
-    "Tgdfgdfgdf.",
+
+  const flowUserNotRegistered= addKeyword(EVENTS.ACTION)
+  .addAnswer(
+    "Para acacceder al bot tienes que registarte.Cual es tu nombre..??",
+    
     { capture: true },
     async (ctx, { state, flowDynamic, gotoFlow }) => {
       const { body } = ctx;
       await state.update({ Registration: true, Name: body });
-      await flowDynamic(`fdgfdgdfgdfgd.`);
-      return gotoFlow(flowRegistered);
+      await flowDynamic(`registarndo en la base de datos `);
+      return gotoFlow(flowmenu);
     }
   );
 
