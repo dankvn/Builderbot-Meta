e438fe097728c867d940e4e327464720f6e8fc1f
kvndan <dannyelctro@gmail.com>
2024-04-25 10:26:09 -0500

    builderbot1✅

--- src/flows/flowUserNotRegistered.js
+++ src/flows/flowUserNotRegistered.js
@@ -2,12 +2,12 @@ import { addKeyword, EVENTS } from '@builderbot/bot';
 import flowRegistered from '../flows/flowRegistered.js';
 
   const flowUserNotRegistered= addKeyword(EVENTS.ACTION).addAnswer(
-    "para aceereder al bot regisatre...👍 .",
+    "Tgdfgdfgdf.",
     { capture: true },
     async (ctx, { state, flowDynamic, gotoFlow }) => {
       const { body } = ctx;
       await state.update({ Registration: true, Name: body });
-      await flowDynamic(`You have been successfully registered.`);
+      await flowDynamic(`fdgfdgdfgdfgd.`);
       return gotoFlow(flowRegistered);
     }
   );
