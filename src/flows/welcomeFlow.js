eaf372a3b3798d17bc751370530a4439d5870673
kvndan <dannyelctro@gmail.com>
2024-04-25 12:14:06 -0500

    builderbo1✅

--- src/flows/welcomeFlow.js
+++ src/flows/welcomeFlow.js
@@ -8,7 +8,7 @@ const googlesheet = new GoogleSheetService(
 );
 
 const welcomeFlow = addKeyword(EVENTS.WELCOME)
-.addAnswer('Welcome!', null, async (ctx, { state, gotoFlow }) => {
+.addAnswer('Welcome! al bot', null, async (ctx, { state, gotoFlow }) => {
  
   const telefono = ctx.from;
   console.log("Número de teléfono:", telefono);
