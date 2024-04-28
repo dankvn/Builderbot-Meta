2f7d1aa0e69e2ab031eec644d64e16e10b7dcf7a
kvndan <dannyelctro@gmail.com>
2024-04-27 22:19:04 -0500

    gpt-4

--- src/flows/welcomeFlow.js
+++ src/flows/welcomeFlow.js
@@ -2,32 +2,15 @@ import { addKeyword, EVENTS } from '@builderbot/bot';
 import flowUserNotRegistered from "./flowUserNotRegistered.js"
 import flowRegistered from "./flowRegistered.js";
 import GoogleSheetService from "../services/sheets/index.js";
-import { GPTFREE } from "gpt4free-plugin";
 
 const googlesheet = new GoogleSheetService(process.env.SHEET_TOKEN);
 
-const gpt = new GPTFREE();
-const welcomeFlow = addKeyword(EVENTS.WELCOME)
-.addAnswer('🤖', null, async (ctx, { state, gotoFlow, flowDynamic }) => {
-  const messages = [
-    { role: "assistant", content: "" },
-    { role: "user", content: text },
-  ];
-  const options = {
-    model: "gpt-4",
-    prompt: ` El cliente escribira y tu "${text}"
-    saludaras de distintas formas al cliente  `,
-  };
-  const text = ctx.body;
-  const telefono = ctx.from;
-  const response = await gpt.chatCompletions(messages, options);
-  
-      console.log(
-        `${new Date()}\nPregunta: ${text} \nRespuesta: ${response}`
-      );
-  
-      await flowDynamic(response);
 
+const welcomeFlow = addKeyword(EVENTS.WELCOME)
+.addAnswer('🤖', null, async (ctx, { state, gotoFlow,  }) => {
+  
+  const telefono = ctx.from;
+  
   console.log("Número de teléfono:", telefono);
       
       const userData = await googlesheet.validatePhoneNumber(telefono);
