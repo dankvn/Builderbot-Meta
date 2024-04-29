41f5b9769b586f209ac7686bcbec797a40337e08
kvndan <dannyelctro@gmail.com>
2024-04-28 22:17:54 -0500

    gpt-4

--- src/flows/flowmenu.js
+++ src/flows/flowmenu.js
@@ -1,12 +1,13 @@
+import { addKeyword } from '@builderbot/bot';
 
-import {  addKeyword, HttpClient, processTemplate } from '@builderbot/bot';
+const flowmenu = addKeyword("PRODUCTOS")
+  .addAnswer([
+    "¿Como podemos ayudarte?",
+    "",
+    "*1-*🛍Realizar *Pedido*",
+    "*2-*👨‍💻Contactar con *Agente* ",
+    
+  ])
+  .addAnswer("Responda con el numero de la opcion!");
 
-const flowmenu = addKeyword('weather')
-    .addAnswer('Vamos a consultar la API del clima', async ({flowDynamic}) => {
-        const response = await HttpClient.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY');
-        const weatherData = response.data;
-        const message = processTemplate('La temperatura en {{city}} es de {{temperature}} grados', weatherData);
-        await flowDynamic(message);
-    });
-  
   export default flowmenu
