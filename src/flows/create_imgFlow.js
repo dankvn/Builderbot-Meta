9760d856ad6e70429eb57aabdc5e59cdce99064a
kvndan <dannyelctro@gmail.com>
2024-05-29 22:18:47 -0500

    g4f

--- src/flows/create_imgFlow.js
+++ src/flows/create_imgFlow.js
@@ -4,60 +4,66 @@ import { addKeyword } from "@builderbot/bot";
 const g4f = new G4F();
 import menuFlow from "./menuFlow.js";
 
-const backOption = `Atras🔙`;
-const translateAgainOption = `Crear🔄`;
+const backOption = `Atras`;
+const translateAgainOption = `Crear`;
 
 const create_imgFlow = addKeyword(["3"])
-.addAnswer("Ingresa un prompt para la creacion de la imaguen")
-.addAction({ capture: true }, async (ctx, { state, flowDynamic }) => {
+  .addAnswer("Ingresa un prompt para la creacion de la imaguen")
+  .addAction({ capture: true }, async (ctx, { state, flowDynamic }) => {
     await state.update({ name: ctx.body });
-    console.log;({ name: ctx.body });
+    console.log;
+    ({ name: ctx.body });
     const text = ctx.body;
-    const base64Image = await g4f.imageGeneration(`${text}`, { 
+    const base64Image = await g4f
+      .imageGeneration(`${text}`, {
         debug: true,
         provider: g4f.providers.Prodia,
         providerOptions: {
-            model: "ICantBelieveItsNotPhotography_seco.safetensors [4e7a3dfd]",
-            samplingSteps: 15,
-            cfgScale: 30
+          model: "ICantBelieveItsNotPhotography_seco.safetensors [4e7a3dfd]",
+          samplingSteps: 15,
+          cfgScale: 30,
+        },
+      })
+      .addAnswer(
+        "¿Quieres realizar otra traducción o regresar al menú anterior?",
+        {
+          buttons: [{ body: translateAgainOption }, { body: backOption }],
         }
-    });	
-    
-    fs.writeFile('image1.jpg', base64Image, { encoding: 'base64' }, async function(err) {
+      )
+      .addAction({ capture: true }, async (ctx, { state, gotoFlow }) => {
+        if (ctx.body === backOption) {
+          await state.update({ attempts: 1 }); // Reset attempts
+          return gotoFlow(menuFlow);
+        }
+        if (ctx.body === translateAgainOption) {
+          await state.update({ attempts: 1 }); // Reset attempts
+          return gotoFlow(create_imgFlow);
+        }
+      });
+
+    fs.writeFile(
+      "image1.jpg",
+      base64Image,
+      { encoding: "base64" },
+      async function (err) {
         if (err) {
-            console.error('Error writing the file: ', err);
+          console.error("Error writing the file: ", err);
         } else {
-            console.log('The image has been successfully saved as image.jpg.');
-            // Leer el archivo y enviarlo como respuesta
-            fs.readFile('image1.jpg', async function(err, data) {
-                if (err) {
-                    console.error('Error reading the file: ', err);
-                } else {
-                    // Enviar la imagen como respuesta
-                    await flowDynamic([
-                        {body:'Aquí está tu imagen:', media: 'image1.jpg', data}
-                    ])
-                }
-            });
+          console.log("The image has been successfully saved as image.jpg.");
+          // Leer el archivo y enviarlo como respuesta
+          fs.readFile("image1.jpg", async function (err, data) {
+            if (err) {
+              console.error("Error reading the file: ", err);
+            } else {
+              // Enviar la imagen como respuesta
+              await flowDynamic([
+                { body: "Aquí está tu imagen:", media: "image1.jpg", data },
+              ]);
+            }
+          });
         }
-    });
-})
+      }
+    );
+  });
 
-.addAnswer("¿Quieres realizar otra traducción o regresar al menú anterior?", {
-    buttons: [{ body: translateAgainOption },{ body: backOption }]
-  })
-  .addAction({ capture: true }, async (ctx, { state, gotoFlow }) => {
-    if (ctx.body === backOption) {
-      await state.update({ attempts: 1 }); // Reset attempts
-      return gotoFlow(menuFlow)
-    } if (ctx.body === translateAgainOption) {
-        await state.update({ attempts: 1 }); // Reset attempts
-        return gotoFlow(create_imgFlow)
-      } 
-  })
-  
-
-  export default create_imgFlow 
-
-
-
+export default create_imgFlow;
